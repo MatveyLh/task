8da624d60ef5eb1899ff7aab7162fa85a78b99fb
Matvey_L <liakh.matvei@mail.ru>
2020-08-19 23:08:11 +0300

    Updates

---
+++ precache-manifest.a897bcc91c6e88dd3417a31f6ffdfe61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56362cdae2868ac108e23d84fe016329",
+    "url": "/ticket/index.html"
+  },
+  {
+    "revision": "a61f1785e2eac3520a6a",
+    "url": "/ticket/static/css/2.a14d84db.chunk.css"
+  },
+  {
+    "revision": "25bbc274ea1980fdea5e",
+    "url": "/ticket/static/css/main.31056674.chunk.css"
+  },
+  {
+    "revision": "a61f1785e2eac3520a6a",
+    "url": "/ticket/static/js/2.8e093456.chunk.js"
+  },
+  {
+    "revision": "de3a2f50d2b7aae39626594c2281ef4c",
+    "url": "/ticket/static/js/2.8e093456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bbc274ea1980fdea5e",
+    "url": "/ticket/static/js/main.68156fa5.chunk.js"
+  },
+  {
+    "revision": "c75fa15880fa34e314c5",
+    "url": "/ticket/static/js/runtime-main.e336fce7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ticket/static/media/logo.5d5d9eef.svg"
+  }
+]);
